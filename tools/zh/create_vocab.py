d2599e82f7822109dcb4d79ae453c4384fc5701b
Jiaxing Qi <jqi@nvidia.com>
2022-03-08 18:15:17 +0800

    add create_vocab.py and jq.zh.vocab

---
+++ tools/zh/create_vocab.py
@@ -0,0 +1,94 @@
+import re
+
+
+def get_ch(filename):
+  tokens = list()
+  with open(filename, 'r', encoding='utf-8') as fin:
+    for line in fin:
+      line = line.strip()
+      if not line:
+        continue
+      _code, token = line.split("\t")
+      tokens.append(token)
+  return tokens
+
+def get_en(filename):
+  tokens = list()
+  with open(filename, 'r', encoding='utf-8') as fin:
+    for line in fin:
+      token = line.strip()
+      if not token:
+        continue
+
+      # exclude entries ending with numbers
+     #if re.match(u'(##)?[a-z]+[a-z]$', token):
+      if re.match(u'(##)?[a-z]*[a-z]$', token):
+       #if re.search(u'[^0-9]', token):
+          tokens.append(token)
+
+  def length(word):
+    return len(word.replace("##", ''))
+
+  tokens.sort(key=length)
+  return tokens
+
+def get_tokens(filename):
+  tokens = list()
+  with open(filename, 'r', encoding='utf-8-sig') as fin:
+    for line in fin:
+      token = line.strip()
+      if not token:
+        continue
+      tokens.append(token)
+  return tokens
+
+# out_file = "en_list.txt"
+def save(filename, tokens):
+  with open(filename, 'w', encoding='utf-8') as fout:
+    for token in tokens:
+      fout.write(token + "\n")
+
+def main():
+  vocab = list()
+  special_tokens = ['[PAD]', '[UNK]', '[CLS]', '[SEP]', '[MASK]']
+  vocab.extend(special_tokens)
+
+  cn_filename = "dict/cn_chars.txt"
+  cn_tokens = get_ch(cn_filename)
+  vocab.extend(cn_tokens)
+
+  cn_punct_filename = "dict/cn_punct_fullwidth.txt"
+  cn_punct_tokens = get_tokens(cn_punct_filename)
+  vocab.extend(cn_punct_tokens)
+
+  google_vocab = "/home/jqi/work/megatron/vocab/bert-base-chinese-vocab.txt"
+  en_tokens = get_en(google_vocab)
+  vocab.extend(en_tokens)
+
+  ascii_symbol_filename = "dict/ascii_symbols.txt"
+  ascii_symbols = get_tokens(ascii_symbol_filename)
+  vocab.extend(ascii_symbols)
+
+  greek_filename = 'dict/greek.txt'
+  greek_tokens = get_tokens(greek_filename)
+  vocab.extend(greek_tokens)
+
+  years = ['2016', '2017', '2018', '2019', '2020', '2021', '2022']
+  num_tokens = list()
+  for num in range(101):
+    num_tokens.append(str(num))
+  for num in years:
+    num_tokens.append(str(num))
+  vocab.extend(num_tokens)
+
+  print("Size of vocab: {}".format(len(vocab)))
+  save("myzh.vocab", vocab)
+
+def test():
+  google_vocab = "/home/jqi/work/megatron/vocab/bert-base-chinese-vocab.txt"
+  en_tokens = get_en(google_vocab)
+  out_file = "en_list.txt"
+  save(out_file, en_tokens)
+
+# test()
+main()
