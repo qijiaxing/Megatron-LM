f3cd00e098bb4c00875b907ce75027126cb3ad59
Jiaxing Qi <jqi@nvidia.com>
2022-03-17 16:53:55 +0800

    add punctuation translate

--- tools/zh/utils.py
+++ tools/zh/utils.py
@@ -1,6 +1,8 @@
 # JQ: split Chinese doc into sentences by regex
 # modified from https://stackoverflow.com/questions/27441191/splitting-chinese-document-into-sentences
 import re
+from zhconv import convert
+
 def zng(para):
     """
       Split a paragraph into sentences by ending symbols
@@ -32,3 +34,22 @@ def stringQ2B(ustring):
 def stringpartQ2B(ustring):
     """把字符串中数字和字母全角转半角"""
     return "".join([Q2B(uchar) if is_Qnumber(uchar) or is_Qalphabet(uchar) else uchar for uchar in ustring])
+
+punct_dict = {
+  '–':'-',   # En Dash (/u2012) -> hyphen
+  '—':'-',   # Em Dash (/u2014) -> hyphen
+  '▼':'',    # Black Down-Pointing Triangle (/u25BC)
+  '℃':'摄氏度',    # Degree Celsius (/u2103)
+  '①':'一', '②':'二', '③': '三', '④':'四', '⑤':'五',
+}
+punct_table = str.maketrans(punct_dict)
+# txt.translate(punct_table)
+
+def translate_punct(ustring):
+  return ustring.translate(punct_table)
+
+def to_zh_cn(string):
+    """
+      JQ: Convert string to Simplified Chinese
+    """
+    return convert(string, "zh-cn")
